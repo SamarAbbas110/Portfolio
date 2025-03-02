6e49a5bb867d3db6ec13aa575bb2a59e61db98f9
Samar Abbas <71007148+SamarAbbas110@users.noreply.github.com>
2025-03-02 17:01:19 +0530

    change intro

--- src/constant/index.js
+++ src/constant/index.js
@@ -9,7 +9,7 @@ export const LINKS = [
 
 export const HERO_CONTENT = {
   greeting: "Hi, I'm Sayyed Samar Abbas 👻",
-  introduction: "Frontend Developer",
+  introduction: "24-year-old tech enthusiast.",
   description: [
     "I am a Frontend Developer with 3 years of experience in the IT industry, including 1 year dedicated to building websites and web applications.",
     "I specialize in JavaScript and have professional expertise in working with React and Redux. I am eager to transition into a Full Stack Developer role, leveraging my skills to contribute across the entire web development lifecycle.",
